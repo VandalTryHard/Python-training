f4283e21c351419f5749da1d9eb7a38964fac0d3
Val <gulenok.valentin@yandex.ru>
2021-10-29 00:26:56 +0600

    Python_for_example_Nicola_Lacy
    Часть 1 "Изучаем Python"
    Основы (task 001-011)
    Инструкция if (task 012-019)
    Строки и числа как переменные (task 020-022), (task 023-026)
    Математические операции (task 027-034)
    Цикл for (task 035-038), (task 039-044)
    Цикл while (task 045-048), (task 049-051)
    Случайные числа (task 052-058), (task 059)
    Черепашья графика (task 060-068)
    Кортежи, списки и словари (task 069-072), (task 073-079)
    Снова о работе со строками (task 080-087)
    Числовые массивы(task 088-095)
    Двумерные списки и словари (task 096-104)
    Чтение и запись текстовых файлов (task 106-110)
    Чтение и запись фалов .csv (task111-113), (task114-115), (task116-117)
    Подпрограммы (task118-120) (task 121-123)
    SQLite, Понятие о реляционных базах данных (task139-140), (task140-141), (task142-143), (task144)

--- Python_for_example_Nicola_Lacy/task144.py
+++ Python_for_example_Nicola_Lacy/task144.py
@@ -6,3 +6,23 @@
 10 - The secret adversary - Agatha Christie - 1921
 11 - The seven dials mystery - Agatha Christie - 1929
 Откройте текстовый файл и убедитесь в том, что программа работает правильно."""
+
+import sqlite3
+import csv
+
+with sqlite3.connect("BookInfo_task141.db") as db: #читает БД
+    cursor = db.cursor()
+
+user_author = input("Enter author: ")
+
+cursor.execute("""SELECT * FROM Books WHERE author = ? """, [user_author]) #Все совбадения
+
+file_ = open("List_Books_task144.csv", "w")
+
+for row in cursor.fetchall(): # преобразует turple в str и записывает в .csv файл
+    row = str(row)
+    new_str = row + "\n"
+    file_.write(new_str)
+
+file_.close()
+db.close()
